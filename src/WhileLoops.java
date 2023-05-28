85aea4c070f84a79e638bc1fa946e4a8bb6f25d4
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-05-28 21:45:58 +0300

    Test commit

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -8,6 +8,7 @@ public class WhileLoops {
         while (myValue <= 5) {
 //            System.out.println("Hello" + myValue);
             ++myValue;
+            // test for git
         }
     }
 }
