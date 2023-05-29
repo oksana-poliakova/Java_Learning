db6c149e0e36dbb106353f528ea52ffd7e9eb955
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-05-29 20:13:53 +0300

    Added string formatting

---
+++ src/StringFormatting.java
@@ -0,0 +1,18 @@
+/**
+ * @author Oksana Poliakova on 29.05.2023
+ * @projectName Lesson1
+ */
+public class StringFormatting {
+    public static void main(String [] args) {
+//        System.out.printf("This is a string, %s \n", "END OF STRING"); // string
+//        System.out.printf("This is a string, %d \n", 5); // digit
+//        System.out.printf("%f This is %s a string, %d \n", 10.4, "Text", 1000);
+        // %10d - occupies the same amount of space
+        // %-10d - if put a minus, then there will be formatting on the left side
+//        System.out.printf("String %10d \n", 123);
+//        System.out.printf("String %-10d \n", 1);
+//        System.out.printf("String %10d \n", 100000);
+        // %.2f - maximum two numbers after the decimal point + rounds a number
+//        System.out.printf("String %.2f \n", 12.3334);
+    }
+}
