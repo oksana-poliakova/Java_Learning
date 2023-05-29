bca6f141f10a6de59824c59625aae4e2970923b3
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-05-29 19:07:33 +0300

    Added the usage of final keyword

---
+++ src/FinalKeyword.java
@@ -0,0 +1,13 @@
+/**
+ * @author Oksana Poliakova on 29.05.2023
+ * @projectName Lesson1
+ */
+public class FinalKeyword {
+    public static void main(String [] args) {
+        final int X = 10;
+    }
+}
+
+class TestFinal {
+    public final int CONSTANT = 0;
+}
