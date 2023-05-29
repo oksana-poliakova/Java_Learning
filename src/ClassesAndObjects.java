4dfa3db77e306a7b9cffc26428c2b02aa57ee6b9
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-05-29 16:08:02 +0300

    Added methods with parameters to the class

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -2,19 +2,39 @@ public class ClassesAndObjects {
     public static void main(String [] args) {
         // object
         Person person1 = new Person();
-        person1.name = "John";
-        person1.age = 20;
+        person1.setNameAndAge("John", 42);
+        person1.sayHello();
 
         Person person2 = new Person();
-        person2.name = "Alice";
-        person2.age = 25;
+        person2.setNameAndAge("Alice", 20);
+        person2.introduce();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Number of years until retirement: " + year2 + " years");
 
         Person [] people = new Person[] { person1, person2 };
-        System.out.println(people[0].age);
+//        System.out.println(people[0].age);
     }
 }
 
 class Person {
     String name;
     int age;
+
+    void introduce() {
+        System.out.println("My name is " + name + " and I'm " + age + " years old");
+    }
+
+    void sayHello() {
+        System.out.println("Hello from " + name);
+    }
+
+    int calculateYearsToRetirement() {
+        int years = 65 - age;
+        return years;
+    }
+
+    void setNameAndAge(String username, int userAge) {
+        name = username;
+        age = userAge;
+    }
 }
