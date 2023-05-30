542508d6d2d38f19366f37138634da955e644f7a
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-05-30 11:53:57 +0300

    Object and .toString()

---
+++ src/ObjectTest.java
@@ -0,0 +1,28 @@
+/**
+ * @author oksanapoliakova on 30.05.2023
+ * @projectName Lesson1
+ */
+public class ObjectTest {
+
+    public static void main(String [] args) {
+        Obj obj1 = new Obj("Some name", 25);
+        obj1.toString(); // returns hash code for this object
+        // printLn, print calls the method toString()
+        System.out.println(obj1);
+    }
+
+}
+
+class Obj {
+    private final String name;
+    private final int age;
+
+    public Obj(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + ", " + age;
+    }
+}
