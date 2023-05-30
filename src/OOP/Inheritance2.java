db77c718f5292d7864af3992464810d5e53697de
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-05-30 13:20:31 +0300

    Inheritance updated

---
+++ src/OOP/Inheritance2.java
@@ -0,0 +1,56 @@
+package OOP;
+
+/**
+ * @author Oksana Poliakova on 30.05.2023
+ * @projectName Lesson1
+ */
+
+public class Inheritance2 {
+    public static void main(String [] args) {
+        Laptop laptop1 = new Laptop("ssd1", "ram1", 2);
+        laptop1.open();
+    }
+}
+
+class Computer {
+    public String ssd;
+    public String ram;
+
+    public Computer(String ssd, String ram) {
+        this.ssd = ssd;
+        this.ram = ram;
+    }
+
+    // Getters
+    public String getRam() {
+        return ram;
+    }
+
+    public String getSsd() {
+        return ssd;
+    }
+
+    public void load() {
+        System.out.println("It's loading...");
+    }
+}
+
+class Laptop extends Computer {
+    
+    private int weight; 
+    public Laptop(String ssd, String ram, int weight) {
+        // constructor from the superclass
+        super(ssd, ram);
+        // custom fields, this is a reference to the current object
+        this.weight = weight;
+        final String ssd1 = getSsd();
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void open() {
+//        System.out.println("Open laptop");
+    }
+}
