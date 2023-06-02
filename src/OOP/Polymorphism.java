475f5b96af14b5f1cada5a4eacd2a8bb84ce179f
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-06-02 14:59:53 +0300

    Polymorphism

---
+++ src/OOP/Polymorphism.java
@@ -0,0 +1,47 @@
+package OOP;
+
+/**
+ * @author Oksana Poliakova on 02.06.2023
+ * @projectName Lesson1
+ */
+public class Polymorphism {
+    public static void main(String[] args) {
+        Mobile[] devices = new Mobile[3];
+        devices[0] = new IPhone();
+        devices[1] = new Android();
+        devices[2] = new IPhone();
+
+        printInformation(devices);
+    }
+
+    public static void printInformation(Mobile[] devices) {
+        for (Mobile device : devices) {
+            device.printInfo();
+            device.makeSound();
+        }
+    }
+}
+
+class Mobile {
+    public void makeSound() {
+        System.out.println("The mobile device makes a sound");
+    }
+
+    public void printInfo() {
+        System.out.println("Print information about mobile");
+    }
+}
+
+class IPhone extends Mobile {
+    @Override
+    public void makeSound() {
+        System.out.println("The iPhone rings");
+    }
+}
+
+class Android extends Mobile {
+    @Override
+    public void makeSound() {
+        System.out.println("The Android device plays a notification sound");
+    }
+}
